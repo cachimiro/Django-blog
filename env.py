a2ac35789b85b3b70a773ea37c7afecab342172e
Ubuntu <ubuntu@ip-172-31-95-123.ec2.internal>
2020-01-09 11:17:21 +0000

    fixiing some issues

---
+++ env.py
@@ -0,0 +1,7 @@
+import os
+
+
+os.environ.setdefault('SECRET_KEY', "'oureohfo947ygh4uhg9v925g088&()Y*&&(&T&&((T**YY^**'" )
+
+
+os.environ.setdefault("DATABASE_URL", "postgres://gjycdsdcnlhfog:504dcb4ea02c3b5e9a45641ca2f463ca4fceced75afca444a762aad4348df09e@ec2-46-137-91-216.eu-west-1.compute.amazonaws.com:5432/dnouum4k7n3ed")
